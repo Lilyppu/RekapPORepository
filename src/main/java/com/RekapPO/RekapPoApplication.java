3eeb72e69c60d72de0b7bab63abe5206f88b922b
lilyppu <lily@ppu.co.id>
2025-06-09 11:34:29 +0700

    first commit

---
+++ src/main/java/com/RekapPO/RekapPoApplication.java
@@ -0,0 +1,13 @@
+package com.RekapPO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekapPoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekapPoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RekapPO/RekapPoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RekapPO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekapPoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
